5c737d87cfbd8744bb7037671cd4d72124d33d7e
PolMarsolUdl <144933485+polMarsol@users.noreply.github.com>
2024-05-18 18:56:43 +0200

    update14

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('¡Formulario enviado!');
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    var loginButton = document.getElementById('loginButton');
+    var loginModal = document.getElementById('loginModal');
+    var closeBtn = document.querySelector('.modal .close');
+
+    loginButton.addEventListener('click', function() {
+        loginModal.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        loginModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == loginModal) {
+            loginModal.style.display = 'none';
+        }
+    });
 });
