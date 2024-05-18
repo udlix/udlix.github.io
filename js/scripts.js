3ad40ef180bdc6424aa94964d53fee2c49b591c3
PolMarsolUdl <144933485+polMarsol@users.noreply.github.com>
2024-05-18 19:15:37 +0200

    update16

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 // scripts.js
 document.addEventListener('DOMContentLoaded', function() {
     var loginButton = document.getElementById('loginButton');
+    var registerButton = document.getElementById('registerButton');
     var loginModal = document.getElementById('loginModal');
-    var closeBtn = document.querySelector('.modal .close');
+    var registerModal = document.getElementById('registerModal');
+    var closeButtons = document.querySelectorAll('.modal .close');
 
     loginButton.addEventListener('click', function() {
         loginModal.style.display = 'block';
     });
 
-    closeBtn.addEventListener('click', function() {
-        loginModal.style.display = 'none';
+    registerButton.addEventListener('click', function() {
+        registerModal.style.display = 'block';
+    });
+
+    closeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            button.parentElement.parentElement.style.display = 'none';
+        });
     });
 
     window.addEventListener('click', function(event) {
         if (event.target == loginModal) {
             loginModal.style.display = 'none';
+        }
+        if (event.target == registerModal) {
+            registerModal.style.display = 'none';
         }
     });
 });
